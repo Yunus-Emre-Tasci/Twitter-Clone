8ebabe2491a53bb1bb435eb20c84e06f6901d7ea
YUNUS EMRE TASCİ <yunusemretasci092@gmail.com>
2022-12-11 22:10:00 +0300

    sidebar

--- src/layout/Sidebar.js
+++ src/layout/Sidebar.js
@@ -1,10 +1,29 @@
 import React from 'react';
-import { HomeIcon } from '../icons/icon';
+import { HomeIcon,
+  ExploreIcon,
+  NotificationsIcon,
+  MessagesIcon,
+  BookmarksIcon,
+  ListsIcon,
+  ProfileIcon,
+  MoreIcon,
+  PopulerIcon,ImageIcon,GIFIcon,PollIcon,EmojiIcon,ScheduleIcon,ReplyIcon,ReTweetIcon,LikeIcon,ShareIcon,SearchIcon } from '../icons/icon';
 
 const Sidebar = () => {
   return (
-    <div className='w-72 bg-blue-200'>
+    <div className='flex flex-col justify-between px-2 w-72 bg-blue-200'>
+      <div>
         <HomeIcon/>
+        <ExploreIcon/>
+        <NotificationsIcon/>
+        <MessagesIcon/>
+        <BookmarksIcon/>
+        <ListsIcon/>
+        <ProfileIcon/>
+        <MoreIcon/>
+      </div>
+      <div>Alt</div>
+        
     </div>
   )
 }
