a0b44c5bcf326cb68306b6af72994933281747db
YUNUS EMRE TASCİ <yunusemretasci092@gmail.com>
2022-12-11 20:36:22 +0300

    layout

--- src/App.js
+++ src/App.js
@@ -2,8 +2,10 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div>
-      <h1 className='bg-purple-500'>App</h1>
+    <div div className = 'flex min-h-screen mx-auto border max-w-7xl' >
+      <div className='w-72 bg-blue-200'>Sidebar</div>
+      <main className='flex-1 bg-purple-200'>Feed</main>
+      <aside className='bg-green-200 w-80'>Timeline</aside>
     </div>
   )
 }
