33e2ac6ca5f738b168a4d14d6085801eef9ca959
YUNUS EMRE TASCİ <yunusemretasci092@gmail.com>
2022-12-12 12:17:25 +0300

    firebase

--- src/layout/Widget.js
+++ src/layout/Widget.js
@@ -1,13 +1,30 @@
-import React from 'react'
+import React from 'react';
+import { SearchIcon} from '../icons/icon';
+import {
+  Timeline
+} from 'react-twitter-widgets'
 
 const Widget = () => {
   return (
-    <div className='bg-green-200 w-80'>
-      <div>
-        
+    <div className='w-80'>
+      <div className='flex items-center space-x-2 p-3 m-3 bg-gray-200 rounded-full text-gray-dark focus-within:bg-white focus-within:ring-1 focus-within:ring-primary-base focus-within:text-primary-base'>
+        <SearchIcon className="w-5 h-5"/>
+        <input type="text" placeholder='Search Twitter' className='placeholder-gray-dark bg-transparent focus:outline-none w-full text-sm' />
       </div>
-      <div>
-        Timeline
+      <div className='mt-5'>
+        < Timeline
+        dataSource = {
+          {
+            sourceType: 'profile',
+            screenName: 'reactjs'
+          }
+        }
+        options = {
+          {
+            height: '500'
+          }
+        }
+        />
       </div>
     </div>
   )
